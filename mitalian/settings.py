fd0ed16c6b6b2395ccf4f7d2d7894bafc164ade2
Dario Gerosa <d.gerosa6@campus.unimib.it>
2017-12-19 15:23:09 +0100

    Styles. Styles. Styles.

--- mitalian/settings.py
+++ mitalian/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mitalian',
+    'material',
 ]
 
 MIDDLEWARE = [
@@ -127,5 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_REDIRECT_URL = '/'
