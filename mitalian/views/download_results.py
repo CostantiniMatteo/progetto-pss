1c0eafd83b997518f79e93501197a148463a0dac
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2017-12-20 15:44:16 +0100

    Fixed migrations

--- mitalian/views/download_results.py
+++ mitalian/views/download_results.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.http import HttpResponseForbidden
+
+from ..models import Collection, Item
 
 def download_results(request, pk):
-    # Create the HttpResponse object with the appropriate CSV header.
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
 
-    writer = csv.writer(response)
+    collection = Collection.objects.get(pk=pk)
+
+    if request.user != collection.user:
+        raise HttpResponseForbidden()
+
+    # Iterate over item_set and write out
+    writer = csv.writer(response, delimiter=' ')
     writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
     writer.writerow(['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
 
