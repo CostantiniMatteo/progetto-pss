ccfdd72af0e7c173c82ec76bda5800086589ff82
Getrio <d.gerosa6@campus.unimib.it>
2017-12-27 01:43:26 +0100

    Auto stash before merge of "master" and "origin/master"
    Tantissimo css e javascript ancora da finire e migliorare. Da sistemare la pagina detail perchè sono quasi le 2 e fra un po' scaglio il pc dalla finestra ;)

---
+++ static/js/circular-bar.js
@@ -0,0 +1,32 @@
+// progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+function circularBar(value) {
+  var bar = new ProgressBar.Circle(document.getElementById("container"), {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 6,
+    trailWidth: 1,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+      autoStyleContainer: false
+    },
+   from: {color: '#FFEA82', width: 4},
+    to: {color: '#ED6A5A', width: 6},
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute('stroke-width', state.width);
+
+      //var value = Math.round(circle.value() * 100);
+
+      circle.setText(value + '%');
+      
+
+    }
+  });
+  bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+  bar.text.style.fontSize = '2rem';
+  bar.animate(value/100);  // Number from 0.0 to 1.0
+}
