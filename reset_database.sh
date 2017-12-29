b7b087200ce8a0a9d72bd71595fbba45b9bc94fe
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2017-12-29 20:10:01 +0100

    Update reset_database script

--- reset_database.sh
+++ reset_database.sh
@@ -4,3 +4,4 @@ psql -c '\c postgres' \
 ./manage.py migrate && \
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'admin')" \
     | python manage.py shell
+rm media/*.jpg
