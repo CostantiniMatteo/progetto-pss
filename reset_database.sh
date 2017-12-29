575fe6002ed184c91e45b24cfedc4af8e0eff8bb
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2017-12-29 19:43:09 +0100

    Add script to reset database. Not tested on linux

---
+++ reset_database.sh
@@ -0,0 +1,6 @@
+psql -c '\c postgres' \
+    -c 'drop database mitalian;' \
+    -c 'create database mitalian with owner admin;' && \
+./manage.py migrate && \
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'admin')" \
+    | python manage.py shell
